e8eb25ae7a4698eacaf39d9cd8633131248501eb
kalamazaad <abdulkalamazaad2447@gmail.com>
2025-08-01 09:43:09 +0530

    v3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 1007}
+  server: {port: 1007},
+  base:'/dora/'
 })
